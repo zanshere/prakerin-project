910e686846890bcf34aff2bd089e2fa53b4eda6f
Fauzan <fauzan224321@gmail.com>
2025-06-27 17:39:20 +0700

    Adding comment

--- config/baseURL.php
+++ config/baseURL.php
@@ -1,12 +1,11 @@
 <?php 
 
 if(!defined('BASE_URL')) {
-    define('BASE_URL', 'https://localhost/git-project/prakerinproject/');
+    define('BASE_URL', 'https://localhost/git-project/prakerin-project/'); // Sesuaikan dengan punya masing masing
 }
 
 function base_url($path = '') {
-    return BASE_URL . ltrim($path, '/');
+    return BASE_URL . ltrim($path, '/'); // penggunaan : base_url('assets/image/test.png')
 }
-
 
 ?>

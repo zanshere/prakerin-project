19eef98ad3ab0027e798ed5d1d17b14c822118ba
Fauzan <fauzan224321@gmail.com>
2025-06-29 21:49:00 +0700

    Removing head HTML and footer

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php 
-include __DIR__ . '/config/baseURL.php';
-
+// Header
+include __DIR__ . '/includes/header.php';
 
 ?>
 
+<!-- Content -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Tailwindcss + DaisyUI -->
-    <link rel="stylesheet" href="<?= base_url('dist/css/style.css') ?>">
-</head>
-<body>
-    <button type="button" class="rounded-md px-6 py-2 btn btn-outline btn-info transition-all hover:text-white duration-300 ease-in-out"><i class="bi bi-alarm"></i>Tes</button>
-</body>
-</html>
+<!-- Footer -->
+<?php include __DIR__ . '/includes/footer.php'; ?>

92a59d936f479314d8ab012f247da3c00bb00e36
Fauzan <fauzan224321@gmail.com>
2025-06-30 21:37:09 +0700

    Fix warning from ESLint

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 // Import AlpineJS
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
 
-window.Alpine = Alpine
-Alpine.start()
+window.Alpine = Alpine;
+Alpine.start();
--- src/js/copy-icons.js
+++ src/js/copy-icons.js
@@ -14,21 +14,24 @@ const destFonts = path.resolve(__dirname, '../../public/icons/fonts');
 
 // Buat folder tujuan jika belum ada
 if (!fs.existsSync(path.dirname(destCSS))) {
-    fs.mkdirSync(path.dirname(destCSS), { recursive: true });
+  fs.mkdirSync(path.dirname(destCSS), { recursive: true });
 }
 
 // Salin file CSS
 fs.copyFileSync(sourceCSS, destCSS);
+// eslint-disable-next-line no-console
 console.log('✔ CSS berhasil disalin ke public/icons/bootstrap-icons.css');
 
 // Salin folder fonts
 if (!fs.existsSync(destFonts)) {
-    fs.mkdirSync(destFonts, { recursive: true });
+  fs.mkdirSync(destFonts, { recursive: true });
 }
 
 fs.readdirSync(sourceFonts).forEach(file => {
-    const src = path.join(sourceFonts, file);
-    const dest = path.join(destFonts, file);
-    fs.copyFileSync(src, dest);
+  const src = path.join(sourceFonts, file);
+  const dest = path.join(destFonts, file);
+  fs.copyFileSync(src, dest);
 });
+
+// eslint-disable-next-line no-console
 console.log('✔ Fonts berhasil disalin ke public/icons/fonts/');

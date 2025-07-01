38f81f1ba3e5e8825a83f5c11ca544893a5185ad
Fauzan <fauzan224321@gmail.com>
2025-07-01 22:14:02 +0700

    Adding header

---
+++ admin/dashboard.php
@@ -0,0 +1,9 @@
+<?php 
+require_once __DIR__ . '/../config/connect.php'; 
+require_once __DIR__ . '/../config/authCheck.php'; 
+
+// Force light theme for this page
+$_SESSION['force_light_theme'] = true;
+include __DIR__ . '/../includes/header.php';
+
+?>

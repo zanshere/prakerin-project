9db02674a12b5587140cf19a8c9ab594e76c9902
Fauzan <fauzan224321@gmail.com>
2025-07-02 21:35:02 +0700

    Adding auth check

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,9 +1,11 @@
 <?php 
 require_once __DIR__ . '/../config/connect.php'; 
-require_once __DIR__ . '/../config/authCheck.php'; 
+require_once __DIR__ . '/../config/authCheck.php';
 
 // Force light theme for this page
 $_SESSION['force_light_theme'] = true;
 include __DIR__ . '/../includes/header.php';
 
+// Update session recovery dengan URL saat ini
+updateSessionRecovery();
 ?>

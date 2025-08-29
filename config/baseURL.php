f98b225f26a2f2886be4f55f1d771a65289ed291
It's Ryuzen <ryuzen@kali.kali>
2025-08-29 21:03:52 +0700

    initial commit

---
+++ config/baseURL.php
@@ -0,0 +1,19 @@
+<?php
+$manualBaseUrl = 'http://localhost:8000'; // Sesuaikan URL disini
+
+if (!defined('BASE_URL')) {
+    define('BASE_URL', rtrim($manualBaseUrl, '/') . '/');
+}
+
+function base_url($path = '') {
+    return BASE_URL . ltrim($path, '/');
+}
+
+function asset_url($path = '') {
+    return base_url('assets/' . ltrim($path, '/'));
+}
+
+function dist_url($path = '') {
+    return base_url('dist/' . ltrim($path, '/'));
+}
+?>
